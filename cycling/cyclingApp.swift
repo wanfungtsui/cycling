9ab707d4580ebd7c47ecb3b9c2bfeb44a8187648
wanfungtsui <wanfung.tsui@yahoo.com>
2024-12-23 20:30:06 +0800

    Initial Commit

---
+++ cycling/cyclingApp.swift
@@ -0,0 +1,17 @@
+//
+//  cyclingApp.swift
+//  cycling
+//
+//  Created by Wanfung Tsui on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct cyclingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cyclingTests/cyclingTests.swift
@@ -0,0 +1,16 @@
+//
+//  cyclingTests.swift
+//  cyclingTests
+//
+//  Created by Wanfung Tsui on 2024/12/23.
+//
+
+import Testing
+
+struct cyclingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
